4a72c50a7d7061ac748bae765a775b28630f27c3
Damian Ostrega <damian.ostrega@gmail.com>
2018-02-22 16:21:21 +0000

    Add analyzer

---
+++ analyzer.rb
@@ -0,0 +1,19 @@
+puts "Hello enter in your first name"
+
+first_name = gets.chomp
+
+puts "Hello enter in your last name"
+
+last_name = gets.chomp
+
+puts "Welcome #{first_name} #{last_name} to the analyzer!"
+
+puts "Your first name has #{first_name.length} charakters and your last name has #{last_name.length} charakters"
+
+puts "In reverse order your name is #{first_name.reverse}"
+
+puts "In reverse order your name is #{first_name.reverse}"
+
+full_name = first_name + " " + last_name
+
+puts "Your full name in reverse is #{full_name.reverse}" 
