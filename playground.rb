e6a963eb02b703044eecd96199bc7a22b201798e
Damian Ostrega <damian.ostrega@gmail.com>
2018-02-22 16:20:49 +0000

    Add playground

---
+++ playground.rb
@@ -0,0 +1,9 @@
+puts "Hello enter in your first name"
+
+first_name = gets.chomp
+
+puts "Hello enter in your last name"
+
+last_name = gets.chomp
+
+puts "Welcome #{first_name} #{last_name} to the playground!"
