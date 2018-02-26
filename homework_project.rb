98d00a1f7dd194072124505941b7fe0b7a08ea2c
Damian Ostrega <damian.ostrega@gmail.com>
2018-02-26 16:06:28 +0000

    Adds methods to code

--- homework_project.rb
+++ homework_project.rb
@@ -1,3 +1,4 @@
+#Assign all cities and codes to hash
 city = {
     'katowice' => 30123,
     'tychy' => 43100,
@@ -11,31 +12,34 @@ city = {
     'rzeszow' => 86653
     }
 
+def cityList(cities)
+  cities.each { |k, v| puts k }
+end
+
+def cityCode(cities,u_resp)
+  result_code = cities[u_resp]
+end
+
 loop do
   puts "Do you want to lookup a city name?(Y/N)"
-
-  answer = gets.chomp
+  answer = gets.chomp.upcase
   if answer != "Y"
     break
   end
 
-puts "What city code would you like to display?" 
-puts ""
-puts "The list of available cities:"
-puts ""
-city_names = city.each { |k, v| puts k }
-puts ""
-puts "Please enter the city name:"
-answer = gets.chomp
-puts ""
-puts "The code for specified city is:"
-result = city.select { |k,v| puts v if k == answer }  
-
+  puts "What city code would you like to display?" 
+  puts ""
+  puts "The list of available cities:"
+  puts ""
+  city_names = cityList(city)
+  puts ""
+  puts "Please enter the city name:"
+  answer = gets.chomp.downcase
+  puts ""
+  result = cityCode(city,answer)
+  if result.nil?
+    puts "Could not find city called #{answer}"
+  else
+    puts "The code for specified city is: #{result}"
+  end
 end
-
-
-
-
-
-
-
